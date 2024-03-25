52f4f511f6ad0ae0d189e1f1433953912cecd2a5
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-25 18:49:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from pyscript import document
 
+
+def runCalculate(event):
+    input_text = document.querySelector("#calc")
+    english = input_text.value
+    output_div = document.querySelector("#output")
+    output_div.innerText = test
