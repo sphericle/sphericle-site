fc2c51a198187245e2e7d35c9c2298f1eb85a19e
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-26 13:21:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ from pyscript import document
 
 
 def runCalculate(event):
-    int(input_text) = document.querySelector("#english"))
-    try:
-        float(input_text)
-        floatCheck = True
-    except ValueError:
-        floatCheck = False
-    if floatCheck == False:
-        output_div = document.querySelector("#output")
-        output_div.innerText = "not a float"
+    float(input_text) = document.querySelector("#english")
+    
     else:
         if input_text.value > 1:
             run = input_text.value / 100
