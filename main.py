214288e7c28d9d4a83ffaa1e861f2423f138560d
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-26 09:09:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ from pyscript import document
 def runCalculate(event):
     run = float()
     try:
-        input_text = float(document.querySelector("#calc"))
-    except:
+        float(s)
+        floatCheck = True
+    except ValueError:
+        floatCheck = False
+    if floatCheck = False:
         output_div = document.querySelector("#output")
-        output_div.innerText = "Please enter a valid run."
-    if input_text.value > 1:
-        run = input_text.value / 100
-    elif input_text.value < 0:
-        if input_text.value > -1:
-            run = input_text.value / 100
-        else:
-            run = input_text
+        output_div.innerText = "test"
     else:
-        run = input_text.value
+        if input_text.value > 1:
+            run = input_text.value / 100
+        elif input_text.value < 0:
+            if input_text.value > -1:
+                run = input_text.value / 100
+            else:
+                run = input_text
+        else:
+            run = input_text.value
         
-    seconds = run * 3600
+        seconds = run * 3600
     
-    output_div = document.querySelector("#output")
-    output_div.innerText = "test"
+        output_div = document.querySelector("#output")
+        output_div.innerText = "test"
