4258aff7a8829284b32bbf7f55438a38d837efda
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-26 13:23:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from pyscript import document
 def runCalculate(event):
     float(input_text) = document.querySelector("#english")
     
-    else:
-        if input_text.value > 1:
+    if input_text.value > 1:
+        run = input_text.value / 100
+    elif input_text.value < 0:
+        if input_text.value > -1:
             run = input_text.value / 100
-        elif input_text.value < 0:
-            if input_text.value > -1:
-                run = input_text.value / 100
-            else:
-                run = input_text
         else:
-            run = input_text.value
+            run = input_text
+    else:
+        run = input_text.value
         
-        seconds = run * 3600
+    seconds = run * 3600
     
-        output_div = document.querySelector("#output")
-        output_div.innerText = seconds
+    output_div = document.querySelector("#output")
+    output_div.innerText = seconds
