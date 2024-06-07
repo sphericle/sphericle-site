8ed28ef587693bc45ff7d7468d6e6fad75b9ecd6
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-06 23:07:51 -0400

    random not secret dumb

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function getUploadDate() {
     let random = 50;
     let button = document.getElementById("uploadAnswer");
     let secret = document.getElementById("uploadSecret")
-    if (button.style.display === "none" && secret !== 50) {
+    if (button.style.display === "none" && random !== 50) {
         button.style.display = "block";
         console.log(random);
-    } else if (button.style.display === "none" && secret === 50) {
+    } else if (button.style.display === "none" && random === 50) {
         secret.style.display = "block"
     } else {
         button.style.display = "none";
