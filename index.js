844cd770ce5cf600e94ebf2ad6e114fa85f6d01d
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-06 23:05:15 -0400

    log random

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function getUploadDate() {
     let secret = document.getElementById("uploadSecret")
     if (button.style.display === "none" && secret !== 50) {
         button.style.display = "block";
+        console.log(random);
     } else if (button.style.display === "none" && secret === 50) {
         secret.style.display = "block"
     } else {
