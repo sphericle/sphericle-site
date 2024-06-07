9c0a7c8714b1bd6203eda0afd3c28c2e9a52c1c7
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-07 15:21:31 -0400

    fix js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function getUploadDate() {
-    let random = Math.floor(Math.random() * 101);
+    let random = Math.floor(Math.random() * 26);
+    const correctNumber = 14
     let button = document.getElementById("uploadAnswer");
     let secret = document.getElementById("uploadSecret")
-    if (button.style.display === "none" && random !== 50) {
+    if (button.style.display === "none" && random !== correctNumber) {
         button.style.display = "block";
         console.log(random);
-    } else if (button.style.display === "none" && random === 50) {
+    } else if (button.style.display === "none" && random === correctNumber) {
         secret.style.display = "block"
     } else {
         button.style.display = "none";
