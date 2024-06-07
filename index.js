c7fb0239b4237436dbc61a918e5520c511ba652b
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-06 23:03:52 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function getUploadDate() {
+    random = Math.floor(Math.random() * 101);
     let button = document.getElementById("uploadAnswer");
-    if (button.style.display === "none") {
+    let secret = document.getElementById("uploadSecret")
+    if (button.style.display === "none" && secret !== 50) {
         button.style.display = "block";
+    } else if (button.style.display === "none" && secret === 50) {
+        secret.style.display = "block"
     } else {
         button.style.display = "none";
+        secret.style.display = "none";
     }
 }
