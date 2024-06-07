42127257281694d70b992deef0d1fbc54c54ddcf
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-06 22:34:21 -0400

    dont show

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getUploadDate() {
     let button = document.getElementById("uploadAnswer")
+    button.style.display = "none";
     if (button.style.display === "none") {
         button.style.display = "block";
     } else {
