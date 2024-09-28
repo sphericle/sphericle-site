5ddea13e1b9e280bafaeb723e77c7c090850bf54
sphericle <86196223+sphericle@users.noreply.github.com>
2024-09-28 11:59:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,10 @@ function getUploadDate() {
     let button = document.getElementById("uploadAnswer");
     let secret = document.getElementById("uploadSecret")
 
-    if (button.style.display === "none" && secret.style.display === "block") {
-        secret.style.display = "none";
-
-    } else if (button.style.display === "none" && random !== correctNumber) {
+    if (button.style.display === "none") {
         button.style.display = "block";
-        console.log(random);
 
-            
-    }else if (button.style.display === "none" && random === correctNumber) {
-        secret.style.display = "block"
-
-
-    }  else {
+    } else {
         button.style.display = "none";
         secret.style.display = "none";
     }
